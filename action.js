a77c53ab66fa753d082161084e0411c837200fff
nazurah0 <nurnazurahhassan3@gmail.com>
2023-09-27 16:29:32 +0800

    Nazz Portfolio 1.0

---
+++ action.js
@@ -0,0 +1,159 @@
+//Baffle
+const text = baffle(".text__glitch");
+text.set({
+    characters: "█▓█ ▒░/▒░ █░▒▓/ █▒▒ ▓▒▓/█ ░█▒/ ▒▓░ █<░▒ ▓/░>",
+    speed: 100
+});
+text.start();
+text.reveal(4000);
+
+//mouse
+
+
+ScrollReveal().reveal(".title", {
+    duration: 3000,
+    move: 0
+  });
+ 
+ScrollReveal().reveal(".about .container", {
+    duration: 3000,
+    origin: "top",
+    distance: "400px",
+    easing: "cubic-bezier(0.5, 0, 0, 1)",
+    rotate: {
+      x: 20,
+      z: -10
+    }
+  });
+
+  ScrollReveal().reveal(".tab", {
+    duration: 2000,
+    scale: 0.85
+  });
+
+  ScrollReveal().reveal(".project-container", {
+    duration: 1000,
+    origin: "left",
+    distance: "300px",
+    easing: "ease-in-out"
+  });
+
+
+/* NAV BAR */
+var app = function () {
+    var body = undefined;
+    var menu = undefined;
+    var menuItems = undefined;
+    var init = function init() {
+        body = document.querySelector('body');
+        menu = document.querySelector('.menu-icon');
+        menuItems = document.querySelectorAll('.nav__list-item');
+        applyListeners();
+    };
+    var applyListeners = function applyListeners() {
+        menu.addEventListener('click', function () {
+            return toggleClass(body, 'nav-active');
+        });
+    };
+    var toggleClass = function toggleClass(element, stringClass) {
+        if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
+    };
+    init();
+}();
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("cd-header").style.top = "0";
+  } else {
+    document.getElementById("cd-header").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+
+
+/* project slider*/
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+//image view
+
+
+//getting all required elements
+const gallery  = document.querySelectorAll(".gallery-img"),
+previewBox = document.querySelector(".preview-box"),
+previewImg = previewBox.querySelector("img"),
+closeIcon = previewBox.querySelector(".icon"),
+currentImg = previewBox.querySelector(".current-img"),
+totalImg = previewBox.querySelector(".total-img"),
+shadow = document.querySelector(".shadow");
+
+window.onload = ()=>{
+    for (let i = 0; i < gallery.length; i++) {
+        totalImg.textContent = gallery.length; //passing total img length to totalImg variable
+        let newIndex = i; //passing i value to newIndex variable
+        let clickedImgIndex; //creating new variable
+        
+        gallery[i].onclick = () =>{
+            clickedImgIndex = i; //passing cliked image index to created variable (clickedImgIndex)
+            function preview(){
+                currentImg.textContent = newIndex + 1; //passing current img index to currentImg varible with adding +1
+                let imageURL = gallery[newIndex].querySelector("img").src; //getting user clicked img url
+                previewImg.src = imageURL; //passing user clicked img url in previewImg src
+            }
+            preview(); //calling above function
+    
+            const prevBtn = document.querySelector(".prev");
+            const nextBtn = document.querySelector(".next");
+            if(newIndex == 0){ //if index value is equal to 0 then hide prevBtn
+                prevBtn.style.display = "none"; 
+            }
+            if(newIndex >= gallery.length - 1){ //if index value is greater and equal to gallery length by -1 then hide nextBtn
+                nextBtn.style.display = "none"; 
+            }
+            prevBtn.onclick = ()=>{ 
+                newIndex--; //decrement index
+                if(newIndex == 0){
+                    preview(); 
+                    prevBtn.style.display = "none"; 
+                }else{
+                    preview();
+                    nextBtn.style.display = "block";
+                } 
+            }
+            nextBtn.onclick = ()=>{ 
+                newIndex++; //increment index
+                if(newIndex >= gallery.length - 1){
+                    preview(); 
+                    nextBtn.style.display = "none";
+                }else{
+                    preview(); 
+                    prevBtn.style.display = "block";
+                }
+            }
+            previewBox.classList.add("show"); 
+            shadow.style.display = "block"; 
+            closeIcon.onclick = ()=>{
+                newIndex = clickedImgIndex; //assigning user first clicked img index to newIndex
+                prevBtn.style.display = "block"; 
+                nextBtn.style.display = "block";
+                previewBox.classList.remove("show");
+                shadow.style.display = "none";
+            }
+        }
+        
+    } 
+}
